4772a1f406211c900139bc64e6057ccbf1fab34b
BT_1N3_11 <sbajnath6151@bths,edu>
2019-02-01 14:41:34 -0500

    eeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -1,10 +1,28 @@
 public class sortAManiaTestRunner {
-    public int[] randomIntsArr(int num){
-        return num;
+
+
+    public static int[] randIntArr(int count){
+        int[] arr = new int[count];
+        for(int i = 0; i<count;i++){
+            arr[i] = (int)(Math.random() * 10001);
+        }
+        return arr;
     }
 
-    public String randomStringArr(int num, int length){
-        return num;
+    public static String[] randomStringArr(int num, int length){
+        String [] arr = new String [num];
+        while (num>0){
+            int i = 0;
+            String s="";
+            while (i<length){
+                char c = (char)((Math.random()*26)+97);
+                s= s+c;
+                i++;
+            }
+            num --;
+            arr[num]=s;
+        }
+        return arr;
     }
 
     public void printArr(){
