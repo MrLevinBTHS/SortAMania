90ce35f346cdd3daa1ebd9ae628d3dfafd8815d1
BT_1N3_12 <jfreund5400@bhts.edu>
2019-02-01 14:15:25 -0500

    things

---
+++ src/Team5SortCompetition.java
@@ -0,0 +1,28 @@
+public class Team5SortCompetition extends SortCompetition   {
+
+    public int challengeOne(int[] arr){
+        return 4;
+    }
+
+    public int challengeTwo(String[] arr, String query){
+        return 3;
+    }
+
+    public int challengeThree(int[] arr){
+        return 2;
+    }
+
+    public int challengeFour(int[][] arr){
+        return 1;
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query){
+        return 0;
+    }
+
+    //Add a custom greeting so your sorter can introduce itself
+    public String greeting(){
+        return "";
+    }
+
+}
